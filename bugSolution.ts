a22b7848557e4798fe4f786d926c014a6de590e6
BugHunter-X <askzenai@gmail.com>
2025-01-21 21:08:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,6 @@
+function greeter(person: string): string {
+  return `Hello, ${person}!`;
+}
+
+let user = ["Jane", "Doe"];
+console.log(greeter(user[0])); // Correct: Accessing the first element of the array
